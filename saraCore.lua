dd598a177eab1f7413b2b04c174acb789fdef6e5
junssekut <arjuna.andio@binus.ac.id>
2022-12-10 09:36:54 +0700

    fix

--- saraCore.lua
+++ saraCore.lua
@@ -1,9 +1,95 @@
 -- asdfghj do not skid bitch
 
-local BotHandler = assert(load(request('GET', 'https://raw.githubusercontent.com/junssekut/saraCore/main/modules/handler/BotHandler.lua'))())
-local Date = assert(load(request('GET', 'https://raw.githubusercontent.com/junssekut/saraCore/main/modules/date/Date.lua'))())
-local TableUtils = assert(load(request('GET', 'https://raw.githubusercontent.com/junssekut/saraCore/main/modules/utils/TableUtils.lua'))())
-local Webhook = assert(load(request('GET', 'https://raw.githubusercontent.com/junssekut/saraCore/main/modules/webhook/Webhook.lua'))())
+local Date = {}
+
+function Date.format(number)
+	return ("%d:%02d:%02d:%02d"):format(
+		math_floor(number / 86400), 
+		math_floor((number % 86400) / 3600), 
+		math_floor((number % 3600) / 60), 
+		math_floor(number % 60))
+end
+
+local BotHandler = {}
+
+function BotHandler.warp(world, id, after, interval, delay)
+    world = world or "exit"
+    id = id or ""
+    after = after or 6
+    interval = interval or 5000
+    delay = delay or 250
+
+    local warpTries = 0
+
+    sleep(delay)
+
+    while getBot().world ~= world:upper() do
+
+        if warpTries == 50 then break end
+
+        if warpTries % after == 0 then
+        	if id == "" then
+            	sendPacket(3, ("action|join_request\nname|%s\ninvitedWorld|0"):format(world))
+	        else
+	            sendPacket(3, ("action|join_request\nname|%s|%s\ninvitedWorld|0"):format(world, id))
+	        end
+        end
+
+        warpTries = warpTries + 1
+
+        if getBot().world == world:upper() then break end
+
+        sleep(interval)
+    end
+
+    if id ~= "" then 
+        warpTries = 0
+
+        while getTile(math.floor(getBot().x / 32), math.floor(getBot().y / 32)).fg == 6 do
+
+            if getTile(math.floor(getBot().x / 32), math.floor(getBot().y / 32)).fg ~= 6 then break end
+
+            if warpTries == 50 then break end
+
+            if warpTries % after == 0 then
+                sendPacket(3, ("action|join_request\nname|%s|%s\ninvitedWorld|0"):format(world, id))
+            end
+
+            warpTries = warpTries + 1
+
+            sleep(interval)
+        end
+    end
+
+    sleep(delay)
+
+    return getBot().world == world:upper()
+end
+
+local TableUtils = {}
+
+function TableUtils.geti(table_value, value)
+	local counter = 1
+
+	for _, v in pairs(table_value) do
+		if v == value then 
+			return counter
+		end
+
+		counter = counter + 1
+	end
+
+	return nil
+end
+
+local Webhook = {}
+
+function Webhook.send(url, message)
+	webhook({
+		url = url,
+		content = message
+	})
+end
 
 return {
 	BotHandler = BotHandler,
